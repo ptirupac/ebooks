a73f0e50ee0f7aed6470c470a33a653ad91ff116
ptirupac <prachu95@gmail.com>
2020-04-25 20:04:46 -0500

    Modified downloader to scrip to work with dynamic directories

--- scripts/downloader.py
+++ scripts/downloader.py
@@ -3,15 +3,19 @@ import requests
 import os
 import sys
 
+
 if sys.version_info[0] < 3:
     raise Exception("Python 3 or a more recent version is required.")
+
+# Getting current file's full path
+dir_path = os.path.dirname(os.path.realpath(__file__))
 
 if os.name == 'nt':
     delimitter = '\\'
 else:
     delimitter = '/'
 
-with open('..{0}data{0}redirected_urls.csv'.format(delimitter), newline='') as f:
+with open('{0}{1}..{1}data{1}redirected_urls.csv'.format(dir_path, delimitter), newline='') as f:
     reader = csv.reader(f)
     books_list = list(reader)
 
@@ -21,9 +25,21 @@ with open('..{0}data{0}redirected_urls.csv'.format(delimitter), newline='') as f
 # 2 --> Original URL
 # 3 --> Redirected URL
 
-print('Getting Redirected URLS: ')
-os.mkdir('..{0}books'.format(delimitter))
+# Creating ../books directory
+try:
+    books_path = '{0}{1}..{1}books'.format(dir_path, delimitter)
+    os.mkdir(books_path)
+    print('Created books directory @ : {0}'.format(books_path))
+except Exception:
+    print('Books directory @ : {0}'.format(books_path))
+
+print('\nGetting Redirected URLS: \n')
+
+
 for i, book in enumerate(books_list):
     print('{0} : {1}'.format(i, book[1]))
     data = requests.get(book[3].replace('/book/', '/content/pdf/'))
-    open('..{0}books{0}{1}.pdf'.format(delimitter, book[1]), 'wb').write(data.content)
+    open('{0}{1}..{1}books{1}{2}.pdf'.format(dir_path, delimitter, book[1]), 'wb').write(data.content)
+    break
+
+print('\n \n ~~~~~~~~~ https://ptirupac.com ~~~~~~~~~')
